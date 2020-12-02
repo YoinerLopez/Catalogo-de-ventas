da52e11877939925f56faee9e8d6f74ec317becb
YoinerLopez <yoineralexis.lopezquintana727@gmail.com>
2020-12-01 21:44:31 -0500

    "Configuro Base de datos"

---
+++ config/database.config.js
@@ -0,0 +1,8 @@
+module.exports = {    
+    url:'mongodb+srv://ingeniero:hola1234@cluster0.kvxkx.mongodb.net/catalogoventas?retryWrites=true&w=majority',
+    options:{
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifedTopoly: true   
+    }
+}
